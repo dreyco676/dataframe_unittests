606db29f912b3b6a859716064b9262e0d87c8e57
John Hogue <john.hogue@genmills.com>
2019-07-16 16:44:41 -0500

    created two data frames to use in unit tests

--- data_prep.py
+++ data_prep.py
@@ -1,5 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv('./datasets/train.csv')
+# read in the data
+df = pd.read_csv('./datasets/titanic.csv.gz', compression='gzip')
 
-df.to_csv('./datasets/titanic.csv.gz', compression='gzip')
+# make 50% of the data as 'historical'
+hist_df = df.sample(frac=.5)
+
+# make 50% of the data as 'new'
+new_df = df.sample(frac=.5)
