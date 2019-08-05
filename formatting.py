7e5103bd99aff671b72b62546bc66ede2a4de6b1
John Hogue <john.hogue@genmills.com>
2019-08-05 10:24:10 -0500

    added full series generation

--- formatting.py
+++ formatting.py
@@ -5,3 +5,12 @@ df = pd.read_csv('./datasets/Metro_Interstate_Traffic_Volume.csv.gz', compressio
 
 print(df.columns)
 df['date_time'] = pd.to_datetime(df['date_time'])
+
+import datetime
+
+start = df['date_time'].min()
+end = df['date_time'].max()
+date_generated = [start + datetime.timedelta(days=x) for x in range(0, (end-start).days)]
+
+full_time_df = pd.DataFrame()
+full_time_df['datetime'] = date_generated
